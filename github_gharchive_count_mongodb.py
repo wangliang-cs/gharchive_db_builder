d675d935b0a9df3492efa8eabbd9db671823f365
Liang Wang <liang.wang.84@gmail.com>
2025-10-08 22:31:51 +0800

    import config

--- github_gharchive_count_mongodb.py
+++ github_gharchive_count_mongodb.py
@@ -1,4 +1,5 @@
 from db.mongodb_gh_count import GHArchiveMongoDBCountUtil
+import config
 
 count_db_util = GHArchiveMongoDBCountUtil(config.get_config("mongodb_conn_str"))
 
@@ -6,4 +7,4 @@ for year in range(2011, 2021):
     for month in range(1, 13):
         if year == 2020 and month >= 11:
             break
-        count_db_util.count_events(f"events_id_{year}-{month:02d}_neo", f"events_count_{year}-{month:02d}")
+        count_db_util.count_events(f"events_id_{year}-{month:02d}", f"events_count_{year}-{month:02d}")
