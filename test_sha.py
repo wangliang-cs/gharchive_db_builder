78a9c02fe700f57cfa226a0719305b59eb846a57
Liang Wang <liang.wang.84@gmail.com>
2025-10-10 11:01:59 +0800

    Update test_sha.py

--- test_sha.py
+++ test_sha.py
@@ -1,8 +1,11 @@
 import utils.gharchive_gzreader as gg
 
 if __name__ == "__main__":
-    test_str = "github:alexreisner/geocoder_github:alexreisner_CreateEvent_2012-12-21T20:56:29Z"
+    test_str = "github:waydetse/waydetse.github.com_github:waydetse_PushEvent_2012-08-31T17:57:27-07:00"
 
     # 生成SHA-256哈希（推荐）
     sha256_hash = gg.generate_sha_hash(test_str)
     print(f"SHA-256: {sha256_hash}")
+    time_str = "2012-08-31T17:57:27-07:00"
+    print(time_str.split('-')[0])
+    print(time_str.split('-')[1])
